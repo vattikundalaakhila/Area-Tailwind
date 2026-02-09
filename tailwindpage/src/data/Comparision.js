5f1a9495cf4f47b134b7ecc7403937241b857857
vattikundalaakhila <akhila.vattikundala.0511@gmail.com>
2026-02-09 16:16:53 +0530

    added few changes

--- tailwindpage/src/data/Comparision.js
+++ tailwindpage/src/data/Comparision.js
@@ -7,37 +7,39 @@ const Comparision = [
     },
 
     {
-        area: {},
-        websurge: {},
-        hyperview: {}
+        area: {available:true, text: "Advanced AI insights"},
+        websurge: {available: true, text:"Basic AI recommendations"},
+        hyperview: {available: false, text: "No AI assistance" }
     }, 
 
     { 
-        area: {},
-        websurge: {},
-        hyperview: {}
+        area: {available: true, text: "Seamless integration"},
+        websurge: {available: true,text: "Restrict customisation"},
+        hyperview: {available: false, text: "Steep learning curve"}
 
     }, 
 
     {
-        area: {},
-        websurge: {},
-        hyperview: {}
+        area: {available: true, text: "Advanced AI insights"},
+        websurge: {available: false, text: "Basic AI insights"},
+        hyperview: {available: false}, text: "No AI assistance"
 
     }, 
 
     {
-        area: {},
-        websurge: {},
-        hyperview: {}
+        area: {available: true, text:"Ultra-fast browsing" },
+        websurge: {available: true, text:"fast browsing" },
+        hyperview: {available: false, text:"moderate speeds" }
 
     }, 
 
     {
-        area: {},
-        websurge: {},
-        hyperview: {}
+        area: {available: true, text: "Full UTF-8 support"},
+        websurge: {available:false, text:  "Potential display errors"},
+        hyperview: {available: false, text: "Partial UTF-8 support"}
 
     }
 
 ];
+
+export default Comparision;
